47a6c6a8085cfe07e05f8799eaf939b43ca4d506
primenumber <prime@kmc.gr.jp>
2015-04-01 04:43:28 +0900

    いい感じに

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <string>
 
+#include <boost/timer.hpp>
+
 constexpr int kana_num = 44;
 constexpr int value_max = 100000;
 
@@ -12,6 +14,21 @@ table input() {
   for (int i = 0; i < kana_num; ++i)
     for (int j = 0; j < kana_num; ++j)
       std::cin >> tb[i][j];
+  return tb;
+}
+
+table erase_loop(table tb) {
+  for (int i = 0; i < kana_num; ++i) {
+    for (int j = 0; j < kana_num; ++j) {
+      if (i != j) {
+        int diff = std::abs(tb[i][j] - tb[j][i]);
+        tb[i][j] -= diff;
+        tb[j][i] -= diff;
+      } else {
+        tb[i][i] -= (tb[i][i] / 2) * 2;
+      }
+    }
+  }
   return tb;
 }
 
@@ -39,20 +56,27 @@ int main(int argc, char **argv) {
   int pos;
   std::cin >> pos;
   auto tb = input();
-  int score_max = -value_max;
+  auto el = erase_loop(tb);
   int next_pos = 0;
-  for (int i = 0; i < kana_num; ++i) {
-    if (tb[pos][i] != 0) {
-      auto cp = tb;
-      --cp[pos][i];
-      int s = -minimax(cp, pos, 4);
-      if (s > score_max) {
-        score_max = s;
-        next_pos = i;
+  boost::timer tm;
+  for (int depth = 3; tm.elapsed() < 1.0; ++depth) {
+    int score_max = -value_max;
+    for (int i = 0; i < kana_num; ++i) {
+      if (tb[pos][i] != 0) {
+        auto cp = el;
+        if (cp[pos][i])
+          --cp[pos][i];
+        else
+          ++cp[i][pos];
+        int s = -minimax(cp, pos, depth);
+        if (s > score_max) {
+          score_max = s;
+          next_pos = i;
+        }
       }
     }
+    std::cerr << "depth: " << depth << ", score: " << score_max << std::endl;
   }
-  std::cerr << score_max << std::endl;
   std::cout << next_pos << std::endl;
   return 0;
 }
